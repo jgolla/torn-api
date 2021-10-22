78e290f7788f188c87c1e9fec9f0a644fcbf5314
Jason Golla <jason@golla.me>
2021-10-22 17:22:12 -0400

    fix(company): add more routes

--- lib/Company.ts
+++ lib/Company.ts
@@ -1,5 +1,5 @@
 import { TornAPIBase } from './TornAPIBase';
-import { ICompany, ICompanyEmployee, ICompanyProfile, ITornApiError } from './Interfaces';
+import { ICompanyDetailed, ICompany, ICompanyEmployee, ICompanyProfile, ITornApiError, INews, ICompanyStock } from './Interfaces';
 import axios from 'axios';
 
 export class Company extends TornAPIBase {
@@ -15,20 +15,20 @@ export class Company extends TornAPIBase {
         return this.apiQueryToArray({ route: 'company', selection: 'companies', jsonOverride: 'company', id: id });
     }
 
-    async detailed(): Promise<undefined> {
-        throw new Error('Method not implemented.');
+    async detailed(): Promise<ICompanyDetailed | ITornApiError> {
+        return this.apiQuery({ route: 'company', selection: 'detailed', jsonOverride: 'company_detailed' });
     }
 
     async employees(id?: string): Promise<ICompanyEmployee[] | ITornApiError> {
         return this.apiQueryToArray({ route: 'company', selection: 'employees', jsonOverride: 'company_employees', id: id }, 'id');
     }
 
-    async news(): Promise<undefined> {
-        throw new Error('Method not implemented.');
+    async news(from?: number, to?: number): Promise<INews[] | ITornApiError> {
+        return this.apiQueryToArray({ route: 'company', selection: 'news', from: from, to: to }, 'id');
     }
 
-    async newsfull(): Promise<undefined> {
-        throw new Error('Method not implemented.');
+    async newsfull(from?: number, to?: number): Promise<INews[] | ITornApiError> {
+        return this.apiQueryToArray({ route: 'company', selection: 'newsfull', jsonOverride: 'news', from: from, to: to }, 'id');
     }
 
     async profile(id?: string): Promise<ICompanyProfile | ITornApiError> {
@@ -42,7 +42,7 @@ export class Company extends TornAPIBase {
         }
     }
 
-    async stock(): Promise<undefined> {
-        throw new Error('Method not implemented.');
+    async stock(): Promise<ICompanyStock[] | ITornApiError> {
+        return this.apiQueryToArray({ route: 'company', selection: 'stock', jsonOverride: 'company_stock' }, 'name');
     }
 }
